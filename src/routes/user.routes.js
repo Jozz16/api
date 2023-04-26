bc854b72955a0c505870cf910f12b2b66d236cdf
Jooz16 <jo633554@gmail.com>
2023-04-25 20:53:05 -0400

    estandarizacion

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,19 +10,19 @@ import sequelize from '../connection-db.js';
   console.log('Tabla Publicacion sincronizada');
 })();
 
-router.post("/register/v1", insertUser);
-router.get("/usuarios", allUsers);
-router.get('/buscar-usuarios/:id', obtenerUsuarioPorId);
-router.post("/login/v1", loginUser);
-router.put("/actualizar-usuario-buscado/:id", actualizarUsuarioBuscado);
-router.delete('/eliminar-usuario/:id', eliminarUsuario);
+router.get("/api/v1/usuarios", allUsers);
+router.get('/api/v1/usuarios/:id', obtenerUsuarioPorId);
+router.post("/api/v1/register", insertUser);
+router.post("/api/v1/login", loginUser);
+router.put("/api/v1/usuario/:id", actualizarUsuarioBuscado);
+router.delete('/api/v1/usuario/:id', eliminarUsuario);
 
 
 
-router.post("/publicacion/v1", createPublicacion);
-router.get("/todas-las-publicaciones", allPublicaciones);
-router.get("/todas-las-publicaciones/autor", allPublicacionesConAutor);
-router.delete('/eliminar-publicacion/:id', eliminarPublicacion);
+router.post("/api/v1/publicacion", createPublicacion);
+router.get("/api/v1/publicaciones", allPublicaciones);
+router.get("/api/v1/publicaciones/autor", allPublicacionesConAutor);
+router.delete('/api/v1/publicacion/:id', eliminarPublicacion);
 
 
 
