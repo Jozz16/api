2ce73ea084a3d96d34c49b6ef7254e938bf915d1
Jozz16 <jo633554@gmail.com>
2023-04-29 00:33:14 -0400

    final

--- src/connection-db.js
+++ src/connection-db.js
@@ -1,11 +1,13 @@
+import * as dotenv from 'dotenv' 
+dotenv.config()
 import Sequelize from "sequelize";
 
 const dbConfig = {
-  username: "postgres",
-  password: "Catateamo123",
-  database: "postgres",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   host: "db.vaguzihkidgsfbfwbiei.supabase.co",
-  port: "5432",
+  port: process.env.DB_PORT,
   dialect: "postgres",
   logging: false,
 };
